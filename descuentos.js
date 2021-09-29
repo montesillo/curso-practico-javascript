81408ffc3aaf6b7ba561e2865bc5cefe3345dc07
Hiram Montesillo <montesillo25@gmail.com>
2021-09-28 22:29:57 -0500

    Unir con Html

--- descuentos.js
+++ descuentos.js
@@ -9,6 +9,17 @@ function calcularPrecioConDescuento(precio, descuento){
     return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultP =  document.getElementById("ResultP");
+    resultP.innerHTML = "El precio con descuento es: $" + precioConDescuento;
+}
 
 // console.log({
 //     precioOriginal, 
