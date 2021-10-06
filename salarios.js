682da33edf02dfc8ac7c1246aa134a5c9d5910a1
Hiram Montesillo <montesillo25@gmail.com>
2021-10-05 23:11:45 -0500

    Salarios

---
+++ salarios.js
@@ -0,0 +1,29 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1500
+});
+colombia.push({
+    name: "Alejandro",
+    salary: 4500
+});
+colombia.push({
+    name: "Zoila",
+    salary: 2000
+});
+colombia.push({
+    name: "Yeri",
+    salary: 15000
+});
+colombia.push({
+    name: "Ocho",
+    salary: 1800
+});
+colombia.push({
+    name: "Berna",
+    salary: 3000
+});
+colombia.push({
+    name: "Ricardo",
+    salary: 1100
+});
